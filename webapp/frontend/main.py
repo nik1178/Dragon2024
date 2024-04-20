29ed2ff8451f3c91fd1b2703c2503787b30c0122
kuscarcek <0000.martin.jereb@gmail.com>
2024-04-20 17:32:57 +0200

    4 graphs + flet done

--- webapp/frontend/main.py
+++ webapp/frontend/main.py
@@ -1,4 +1,4 @@
-import flet
+import flet as ft
 import time
 import matplotlib
 import matplotlib.pyplot as plt
@@ -9,37 +9,110 @@ from flet.matplotlib_chart import MatplotlibChart
 matplotlib.use("svg")
 
 
-def main(page: flet.Page):
+def main(page: ft.Page):
+
+    page.theme_mode = ft.ThemeMode.SYSTEM
+    page.title = "rAId"
+    page.scroll = "adaptive"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.fonts = {
+        
+    }
+
+    page.add(
+        ft.Text("R", size=50, weight=ft.FontWeight.W_500, 
+                spans=[ft.TextSpan("AI", ft.TextStyle(size=50, weight=ft.FontWeight.W_700, color="#3c8062")), ft.TextSpan("d", ft.TextStyle(size=50, weight=ft.FontWeight.W_500))]),
+    )
+
+    page.update()
+
+
     def generate_data():
         return np.random.rand()
 
-    def update_plot(frame):
-        x_data.append(time.time() - start_time)  
-        y_data.append(generate_data())           
-        ax.clear()
-        ax.plot(x_data, y_data)
-        ax.set_xlabel('Time (s)')
-        ax.set_ylabel('Poraba')
-
-    fig, ax = plt.subplots()
-    x_data, y_data = [], []
+    fig1, ax1 = plt.subplots()
+    fig2, ax2 = plt.subplots()
+    fig3, ax3 = plt.subplots()
+    fig4, ax4 = plt.subplots()
+    x1_data, y1_data = [], []
+    x2_data, y2_data = [], []
+    x3_data, y3_data = [], []
+    x4_data, y4_data = [], []
 
     start_time = time.time()
 
-    #ani = animation.FuncAnimation(fig, update_plot, interval=1000)
-    #plt.show()
-    update_plot(fig)
-    graf1 = MatplotlibChart(fig, isolated=True, expand=True)
-    page.add(graf1)
+    
+    graf1 = MatplotlibChart(fig1, isolated=True, expand=True)
+    graf2 = MatplotlibChart(fig2, isolated=True, expand=True)
+    graf3 = MatplotlibChart(fig3, isolated=True, expand=True)
+    graf4 = MatplotlibChart(fig4, isolated=True, expand=True)
+    
+    page.add(
+        ft.Row([
+            graf1,
+            graf2
+        ]),
+        ft.Row([
+            graf3,
+            graf4
+        ])
+    )
+
+
+    page.add(
+        ft.Row([
+            ft.Text("Click on this button to start analysis: ", size=20, weight=ft.FontWeight.W_500),
+        ])
+    )
+
+    page.update()
+
+    def update_graph1():
+        x1_data.append(time.time() - start_time)  
+        y1_data.append(generate_data())           
+        ax1.clear()
+        ax1.plot(x1_data, y1_data)
+        ax1.set_xlabel('Time (s)')
+        ax1.set_ylabel('Poraba')
+
+    def update_graph2():
+        x2_data.append(time.time() - start_time)  
+        y2_data.append(generate_data())           
+        ax2.clear()
+        ax2.plot(x2_data, y2_data)
+        ax2.set_xlabel('Time (s)')
+        ax2.set_ylabel('Poraba')
+
+    def update_graph3():
+        x3_data.append(time.time() - start_time)  
+        y3_data.append(generate_data())           
+        ax3.clear()
+        ax3.plot(x3_data, y3_data)
+        ax3.set_xlabel('Time (s)')
+        ax3.set_ylabel('Poraba')
+
+    def update_graph4():
+        x4_data.append(time.time() - start_time)  
+        y4_data.append(generate_data())           
+        ax4.clear()
+        ax4.plot(x4_data, y4_data)
+        ax4.set_xlabel('Time (s)')
+        ax4.set_ylabel('Poraba')
+    
 
     while True:
-        x_data.append(time.time() - start_time)  
-        y_data.append(generate_data()) 
-        ax.plot(x_data, y_data)   
         time.sleep(1)
+        update_graph1()
+        update_graph2()
+        update_graph3()
+        update_graph4()
         graf1.update()
+        graf2.update()
+        graf3.update()
+        graf4.update()
 
     
     
 
-flet.app(target=main)
+ft.app(target=main)
