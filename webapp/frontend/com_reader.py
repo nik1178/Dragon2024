6b3ed30a01d65f1762248ba44a1f53f0818ef268
Nik Jenic <jenic.nik@gmail.com>
2024-04-20 19:07:50 +0200

    Fake data1

--- webapp/frontend/com_reader.py
+++ webapp/frontend/com_reader.py
@@ -1,5 +1,6 @@
 import serial
 import threading
+import random
 
 # Define global variables
 # ser = serial.Serial("COM7", 115200, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
@@ -43,7 +44,7 @@ def receive_data():
 
 def fake_data():
     while True:
-        new_data.append([1,2,3,4])
+        new_data.append([random.randint(1,140),random.randint(1,140),random.randint(1,140),random.randint(1,140)])
         time.sleep(1)
 
 def get_data():
